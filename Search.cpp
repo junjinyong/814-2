8325befa3c831ef699954524466c685290318565
junjinyong <junjinyong@snu.ac.kr>
2023-08-04 05:50:37 +0900

    Implemented skeleton code for reader/writer lock

--- Search.cpp
+++ Search.cpp
@@ -1,3 +1,24 @@
-//
-// Created by chowd on 2023-08-04.
-//
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+
+using namespace std;
+
+typedef shared_mutex Lock;
+typedef unique_lock <Lock> WriteLock;
+typedef shared_lock <Lock> ReadLock;
+
+Lock myLock;
+
+int solution[8][14];
+int data[20000];
+
+void read() {
+    ReadLock r_lock(myLock);
+    //Do reader stuff
+}
+
+void write() {
+    WriteLock w_lock(myLock);
+    //Do writer stuff
+}
--- main.cpp
+++ main.cpp
@@ -2,27 +2,14 @@
 #include <thread>
 #include <mutex>
 #include <shared_mutex>
+#include "Search.cpp"
 
 using namespace std;
 
-typedef shared_mutex Lock;
-typedef unique_lock <Lock> WriteLock;
-typedef shared_lock <Lock> ReadLock;
 
-Lock myLock;
-
-void ReadFunction() {
-    ReadLock r_lock(myLock);
-    //Do reader stuff
-}
-
-void WriteFunction() {
-    WriteLock w_lock(myLock);
-    //Do writer stuff
-}
 
 int main() {
-    cout << "number of logical processors: " << std::thread::hardware_concurrency() << endl;
+    cout << "number of logical processors: " << thread::hardware_concurrency() << endl;
     cout << "thread id: " << this_thread::get_id() << endl;
     
     
